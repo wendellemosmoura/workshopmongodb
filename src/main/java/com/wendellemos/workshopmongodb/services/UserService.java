e589543d8957a1a970fcd5aedba0ee574c704445
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-06-14 19:01:46 -0300

    Inserção de usuário com POST

--- src/main/java/com/wendellemos/workshopmongodb/services/UserService.java
+++ src/main/java/com/wendellemos/workshopmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.wendellemos.workshopmongodb.services;
 
 import com.wendellemos.workshopmongodb.domain.User;
+import com.wendellemos.workshopmongodb.dto.UserDTO;
 import com.wendellemos.workshopmongodb.repository.UserRepository;
 import com.wendellemos.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
