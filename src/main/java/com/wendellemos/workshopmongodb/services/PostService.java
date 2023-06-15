372156f56c5e75f6c1746d64efe36b31bb6bee62
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-06-15 12:15:41 -0300

    Obtendo um post por id

---
+++ src/main/java/com/wendellemos/workshopmongodb/services/PostService.java
@@ -0,0 +1,28 @@
+package com.wendellemos.workshopmongodb.services;
+
+import com.wendellemos.workshopmongodb.domain.Post;
+import com.wendellemos.workshopmongodb.domain.User;
+import com.wendellemos.workshopmongodb.repository.PostRepository;
+import com.wendellemos.workshopmongodb.repository.UserRepository;
+import com.wendellemos.workshopmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public Post findById(String id) {
+        Optional<Post> obj = postRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
