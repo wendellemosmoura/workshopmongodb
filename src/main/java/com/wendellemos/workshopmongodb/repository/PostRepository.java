36204f7c849ed9ad1bea1e524d34d98ff219bc00
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-06-16 11:55:14 -0300

    Consulta simples com @Query

--- src/main/java/com/wendellemos/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/wendellemos/workshopmongodb/repository/PostRepository.java
@@ -2,10 +2,13 @@ package com.wendellemos.workshopmongodb.repository;
 
 import com.wendellemos.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> serchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
