80c54b46bcd4b4b1abff12fa8d30cd7fa3ca0374
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-06-16 11:28:15 -0300

    Consulta simples com query methods

--- src/main/java/com/wendellemos/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/wendellemos/workshopmongodb/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.wendellemos.workshopmongodb.repository;
 import com.wendellemos.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/wendellemos/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/wendellemos/workshopmongodb/resources/PostResource.java
@@ -2,13 +2,13 @@
 package com.wendellemos.workshopmongodb.resources;
 
 import com.wendellemos.workshopmongodb.domain.Post;
+import com.wendellemos.workshopmongodb.resources.util.URL;
 import com.wendellemos.workshopmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -21,5 +21,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
